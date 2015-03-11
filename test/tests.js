7266d540e19109219e704d39e6c189d92bb03ee1
maael <mae.me.uk@gmail.com>
2015-03-11 17:30:29 +0000

    Update algorithm to return fittest individual after termination, update tests with sin test with no timeout

--- test/tests.js
+++ test/tests.js
@@ -3,11 +3,15 @@ var genetic = require('..'),
     should = chai.should();
 
 describe('node-genetic-algorithm', function() {
-    describe('sin test', function() {
-        genetic(function(x, y){
-            return (Math.sin(2 * x) * x + Math.sin(x + y) * y)
-        }, [-Math.PI, 6 * Math.PI], {
-            parameters: ['x', 'y']
+    describe('Math.sin(2 * x) * x + Math.sin(x + y) * y max between -pi, 6pi', function() {
+        it('should produce a solution close to the maximum', function() {
+            this.timeout(0);
+            var solution = genetic(function(x, y){
+                return (Math.sin(2 * x) * x + Math.sin(x + y) * y)
+            }, [-Math.PI, 6 * Math.PI], {
+                parameters: ['x', 'y']
+            });
+            console.log(solution);
         });
     });
 });
